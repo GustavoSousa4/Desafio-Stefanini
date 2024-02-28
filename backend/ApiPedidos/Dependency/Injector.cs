815d7e98bf5e022ff4cf53edbcba84f97dfa201f
Gustavo Sousa <TR042391@minhati.com.br>
2024-02-28 09:37:12 -0300

    Configurações de injeção de dependencia

---
+++ backend/ApiPedidos/Dependency/Injector.cs
@@ -0,0 +1,26 @@
+﻿using Application.Pedido.Service;
+using Application.Produto.Service;
+using Domain.Respositories;
+using Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dependency
+{
+    public static class Injector
+    {
+        public static void AddLocalServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region Repository
+            services.AddTransient<IProdutoRepository, ProdutoRepository>();
+            services.AddTransient<IPedidoRepository, PedidoRepository>();
+            services.AddTransient<IItensPedidoRepository, ItensPedidoRepository>();
+            #endregion
+
+            #region Service
+            services.AddTransient<IProdutoService, ProdutoService>();
+            services.AddTransient<IPedidoService, PedidoService>();
+            #endregion
+        }
+    }
+}
